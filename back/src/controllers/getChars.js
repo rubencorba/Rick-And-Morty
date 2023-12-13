c21d45ccd5b96ed9ee5d7f9285c515eebaff2b33
Rubén Corbalán Paz <rubencorba@gmail.com>
2023-12-12 23:48:08 -0300

    get chars advanced

--- back/src/controllers/getChars.js
+++ back/src/controllers/getChars.js
@@ -1,32 +1,20 @@
 const getChars= async (req,res)=>{
 
 
-    for (let i = 0; i < 6; i++) {
-        const id=Math.floor(Math.random()*400);
-        const {data}= await axios(`https://rym2.up.railway.app/api/character/${id}?key=pi-rubencorba`)
-        setCharacters2((oldChars) => [...oldChars, {image:data.image,id:{ide:data.id,key:i}}]);
-        setCharacters2((oldChars) => [...oldChars, {image:data.image,id:{ide:data.id,key:i+1}}]);
-    }
-    /* try {
-        const {id} =req.params;
-        const {data}=await axios.get(`https://rym2.up.railway.app/api/character/${id}?key=pi-rubencorba`)
-        
-        if (data.name){
-            const character ={
-                id: data.id,
-                name: data.name,
-                status: data.status,
-                origin: data.origin,
-                species: data.species,
-                image: data.image,
-                gender: data.gender
-            }
-            return res.status(200).json(character)
+    try {
+        for (let i = 0; i < 6; i++) {
+            const id=Math.floor(Math.random()*400);
+            const {data}= await axios(`https://rym2.up.railway.app/api/character/${id}?key=pi-rubencorba`)
+            setCharacters2((oldChars) => [...oldChars, {image:data.image,id:{ide:data.id,key:i}}]);
+            setCharacters2((oldChars) => [...oldChars, {image:data.image,id:{ide:data.id,key:i+1}}]);
         }
-        return res.status(404).send('Not found')
+        
+
+            return res.status(200).json(character)
+            
     } catch (error) {
         (error)=> res.status(500).send(error.message)
-    } */
+    }
         
         
         
