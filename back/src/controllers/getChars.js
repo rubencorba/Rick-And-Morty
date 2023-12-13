6f39b2fb321198fe3ed6bedc472ee50245f7420b
Rubén Corbalán Paz <rubencorba@gmail.com>
2023-12-12 23:56:07 -0300

    getChars para juego

--- back/src/controllers/getChars.js
+++ back/src/controllers/getChars.js
@@ -4,14 +4,15 @@ const getChars= async (req,res)=>{
     try {
         for (let i = 0; i < 6; i++) {
             const id=Math.floor(Math.random()*400);
-            const {data}= await axios(`https://rym2.up.railway.app/api/character/${id}?key=pi-rubencorba`)
+            /* const {data}= await axios(`https://rym2.up.railway.app/api/character/${id}?key=pi-rubencorba`) */
+            const {data}= await axios(`https://rickandmortyapi.com/api/character/${id}`)
             setCharacters2((oldChars) => [...oldChars, {image:data.image,id:{ide:data.id,key:i}}]);
             setCharacters2((oldChars) => [...oldChars, {image:data.image,id:{ide:data.id,key:i+1}}]);
         }
         
 
             return res.status(200).json(character)
-            
+
     } catch (error) {
         (error)=> res.status(500).send(error.message)
     }
