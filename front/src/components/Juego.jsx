ea6edfe14b94a0b2535180e8cefe6a37dc582b9e
Rubén Corbalán Paz <rubencorba@gmail.com>
2023-12-18 23:09:48 -0300

    cambiando Juego.jsx

--- front/src/components/Juego.jsx
+++ front/src/components/Juego.jsx
@@ -18,25 +18,24 @@ const Juego=()=>{
         /* if (d===0){
             d=1;
         } */
-        mezclar();
+        /* mezclar(); */
     },[characters2])
 
     const barajar= async ()=>{
         dispatch(getCharsByGame())
-        for (let i = 0; i < 6; i++) {
+        /* for (let i = 0; i < 6; i++) {
             const id=Math.floor(Math.random()*400);
             const {data}= await axios(`https://rym2.up.railway.app/api/character/${id}?key=pi-rubencorba`)
             setCharacters2((oldChars) => [...oldChars, {image:data.image,id:{ide:data.id,key:i}}]);
             setCharacters2((oldChars) => [...oldChars, {image:data.image,id:{ide:data.id,key:i+1}}]);
-        }
+        } */
     } 
     
-    const mezclar= ()=>{
-        /* if (d===0) {
-            d=1 */
+    /* const mezclar= ()=>{
+        
         const mezcladas=characters2.sort(()=>Math.random() - 0.5)
-        setCharacters2(mezcladas)/* } */
-    }
+        setCharacters2(mezcladas)
+    } */
     const reiniciar= ()=>{
         setCharacters2([])
         dispatch(reiniciarJuego())
@@ -61,7 +60,7 @@ const Juego=()=>{
     const cartasBocaArriba = useSelector((state) => state.cartasBocaArriba);
     
 
-
+    const charsByGame = useSelector((state) => state.charsByGame);
     
     
     return (
@@ -70,19 +69,19 @@ const Juego=()=>{
             
                 <div>
                     
-                     {characters2.length!==0
+                     {/* characters2 */charsByGame.length!==0
                 ?<button /* disabled={characters2.length===0} */ onClick={()=>reiniciar()}>reiniciar</button>
                 :<button /* disabled={characters2.length!==0} */ onClick={()=>barajar()}>barajar</button>}
 
                         <div className="card-grid">
-                        {characters2.length===12? (
-                        characters2.map(({image,id,key})=>{
+                        {/* characters2 */charsByGame.length===12? (
+                        /* characters2 */charsByGame.map(({image,id,key})=>{
                             return <CardDeJuego imagenFoto={image} id={id} /* key={key} *//>
                         })
                         ):null}
                         </div>
                         <div>
-                            {characters2.length>1&&characters2.length<12?
+                            {/* characters2 */charsByGame.length>1&&/* characters2 */charsByGame.length<12?
                             <img /* className='cardJuegoStyle' */src={portal} alt="portal" />:null}
                         </div>
                 
@@ -90,7 +89,7 @@ const Juego=()=>{
                 </div>          
             :<div>
                 <div>
-                    {characters2.length!==0
+                    {/* characters2 */charsByGame.length!==0
                     ?<button /* disabled={characters2.length===0} */ onClick={()=>reiniciar()}>reiniciar</button>
                     :<button /* disabled={characters2.length!==0} */ onClick={()=>barajar()}>barajar</button>}
                 </div>
