006f6753537d81b632a148adf54d274f0665e2be
Rubén Corbalán Paz <rubencorba@gmail.com>
2024-01-03 21:21:05 -0300

    borrando comentarios

--- back/src/controllers/getChars.js
+++ back/src/controllers/getChars.js
@@ -2,15 +2,11 @@ let axios = require('axios');
 
 const getChars= async (req,res)=>{
 
-    /* const [characters2,setCharacters2]= useState([]) */
     try {
         let charsForGame=[];
         for (let i = 0; i < 6; i++) {
             const id=Math.floor(Math.random()*400);
             const {data}= await axios.get(`https://rym2.up.railway.app/api/character/${id}?key=pi-rubencorba`)
-            /* const {data}= await axios(`https://rickandmortyapi.com/api/character/${id}`) */
-            /* setCharacters2((oldChars) => [...oldChars, {image:data.image,id:{ide:data.id,key:i}}]);
-            setCharacters2((oldChars) => [...oldChars, {image:data.image,id:{ide:data.id,key:i+1}}]); */
             
             charsForGame.push({image:data.image,id:{ide:data.id,key:i+1}})
             charsForGame.push({image:data.image,id:{ide:data.id,key:i+1}})
