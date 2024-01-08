0d1cbb525e0219ac922fcb829909ce1bf35477e2
Rubén Corbalán Paz <rubencorba@gmail.com>
2024-01-07 23:31:38 -0300

    un cambio

--- back/src/DB_connection.js
+++ back/src/DB_connection.js
@@ -27,6 +27,7 @@ const { User, Favorite } = sequelize.models;
 User.belongsToMany(Favorite, {through: 'user_favorite'});
 Favorite.belongsToMany(User, {through: 'user_favorite'});
 
+
 module.exports = {
    User,
    Favorite,
